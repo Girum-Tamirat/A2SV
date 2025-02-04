1b4743dfc6c2c37cc67d5f4ed17776dd22f19bb3
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-02-04 17:37:22 +0300

    Created 'Thearte Square' by A2SV Hub

---
+++ 04-Feb-2025/Thearte Square 240398.py
@@ -0,0 +1,12 @@
+# Problem: Thearte Square - https://codeforces.com/problemset/problem/1/A
+
+import math
+
+def number_of_flagstones(n, m, a):
+    fl = math.ceil(n / a)
+    fw = math.ceil(m / a)
+
+    return fl * fw
+if __name__ == '__main__':
+    n, m, a = map(int, input().split())
+    print(number_of_flagstones(n, m, a))
