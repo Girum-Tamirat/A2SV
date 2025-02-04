092c70aff58d90657088183c0c20374bef7a90e8
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-02-04 18:04:09 +0300

    Created 'Arithmetic Operator' by A2SV Hub

---
+++ 04-Feb-2025/Arithmetic Operator 240455.py
@@ -0,0 +1,14 @@
+# Problem: Arithmetic Operator - https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+
+def stub(a, b):
+    x = a + b
+    y = a - b
+    z = a * b
+    print (x)
+    print (y)
+    print (z)
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    stub(a, b)
