cdadc3f63aaeb3922a55517b992a4f4ab9f829f8
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-02-04 18:21:19 +0300

    Created 'Domino Piling' by A2SV Hub

---
+++ 04-Feb-2025/Domino Piling 240525.py
@@ -0,0 +1,7 @@
+# Problem: Domino Piling - https://codeforces.com/problemset/problem/50/A
+
+def max_dominoes (m, n):
+    return (m * n) // 2
+if __name__ == '__main__':
+    m, n = map(int, input().split())
+    print(max_dominoes(m, n))
