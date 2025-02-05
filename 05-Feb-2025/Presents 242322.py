d5f1bc97ed9af69901d34b977a35b166b96f9e61
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-02-05 11:37:49 +0300

    Created 'Presents' by A2SV Hub

---
+++ 05-Feb-2025/Presents 242322.py
@@ -0,0 +1,9 @@
+# Problem: Presents - https://codeforces.com/problemset/problem/136/A
+
+if __name__ == '__main__':
+    n = int(input())
+    p = list(map(int, input().split()))
+    result = [0] * n
+    for i in range(n):
+        result[p[i] - 1] = i + 1
+    print(" ".join(map(str, result)))
