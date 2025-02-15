49836782620c884cef8decc1c5127b4a59b3d912
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-02-15 19:12:14 +0300

    Created '4Sum II' by A2SV Hub

---
+++ 15-Feb-2025/4Sum II 267150.py
@@ -0,0 +1,6 @@
+# Problem: 4Sum II - https://leetcode.com/problems/4sum-ii/
+
+class Solution:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+                nums1nums2 = collections.Counter(a + b for a in nums1 for b in nums2)
+                return sum(nums1nums2[-c-d] for c in nums3 for d in nums4)
