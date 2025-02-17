c8f00da26332b03a93f85b7a1b9366c8917f3eaf
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-02-17 21:27:27 +0300

    Created 'C - Barking Password' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/C - Barking Password 269567.py
@@ -0,0 +1,17 @@
+# Problem: C - Barking Password - https://codeforces.com/gym/588094/problem/C
+
+def barkingpassword(p, n, c):
+    if p in c:
+        return "YES"
+    for i in range(n):
+        for j in range(n):
+            if c[i][1] == p[0] and c[j][0] == p[1]:
+                return "YES"
+    return "NO"
+p = input()
+n = int(input())
+c = [input() for _ in range(n)]
+
+result = barkingpassword(p, n, c)
+
+print(result)
