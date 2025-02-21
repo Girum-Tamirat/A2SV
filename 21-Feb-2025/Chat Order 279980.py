14faa948fc9f24ca03364d83848a1ea2c794e53d
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-02-21 23:43:01 +0300

    Created 'Chat Order' by A2SV Hub

---
+++ 21-Feb-2025/Chat Order 279980.py
@@ -0,0 +1,9 @@
+# Problem: Chat Order - https://codeforces.com/problemset/problem/637/B
+
+n = int(input())
+ins = [input() for _ in range(n)]
+seen = {}
+for name in ins:
+    seen.pop(name, None)
+    seen[name] = None
+print("\n".join(reversed(seen.keys())))
