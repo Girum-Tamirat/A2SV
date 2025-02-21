4cb7c3aeaafe79dd1da849f03528e9a827c10731
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-02-21 22:16:58 +0300

    Created 'A. Beautiful Matrix' by A2SV Hub

---
+++ 21-Feb-2025/A. Beautiful Matrix 279657.py
@@ -0,0 +1,7 @@
+# Problem: A. Beautiful Matrix - https://codeforces.com/problemset/problem/263/A
+
+for i in range(5):
+    row = input().split()
+    if '1' in row:
+        print(abs(2 - i) + abs(2 - row.index('1')))
+    
