30413f0ec39c55f391ad6b01570ed80d5b3271e1
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-03-01 20:16:39 +0300

    Created 'Fox And Snake' by A2SV Hub

---
+++ 01-Mar-2025/Fox And Snake 287707.py
@@ -0,0 +1,12 @@
+# Problem: Fox And Snake - https://codeforces.com/problemset/problem/510/A
+
+n, m = map(int, input().split())
+
+for r in range(1, n+1):
+    if r % 2 == 1:
+        print('#' * m)
+    else:
+        if r % 4 == 0:
+            print('#' + '.' * (m-1))
+        else:
+            print('.' * (m-1) + '#')
