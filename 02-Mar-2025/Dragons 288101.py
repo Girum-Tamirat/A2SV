93324e760d3b6d81aba8617f9b8f48a0c194f779
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-03-02 15:24:55 +0300

    Created 'Dragons' by A2SV Hub

---
+++ 02-Mar-2025/Dragons 288101.py
@@ -0,0 +1,18 @@
+# Problem: Dragons - https://codeforces.com/problemset/problem/230/A
+
+s, n = map(int, input().split())
+dragons = []
+for _ in range(n):
+    x, y = map(int, input().split())
+    dragons.append((x, y))
+
+dragons.sort()
+
+for x, y in dragons:
+    if s > x:
+        s += y
+    else:
+        print("NO")
+        exit()
+
+print("YES")
