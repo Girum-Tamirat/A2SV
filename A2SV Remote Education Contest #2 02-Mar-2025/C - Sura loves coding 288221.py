e3ad1bd53f19801e8bf2b0eac0e5fe43eeba1276
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-03-02 19:48:28 +0300

    Created 'C - Sura loves coding' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 02-Mar-2025/C - Sura loves coding 288221.py
@@ -0,0 +1,9 @@
+# Problem: C - Sura loves coding - https://codeforces.com/gym/586622/problem/C
+
+n = int(input())
+s = input()
+org = []
+for i in range(n):
+    pos = i // 2
+    org.insert(pos, s[n - i - 1])
+print("".join(org))
