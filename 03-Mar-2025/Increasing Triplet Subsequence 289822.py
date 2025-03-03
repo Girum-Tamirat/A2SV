8ca27c429604025b6b3a8eb76f0d2247f8287730
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-03-03 19:29:51 +0300

    Created 'Increasing Triplet Subsequence' by A2SV Hub

---
+++ 03-Mar-2025/Increasing Triplet Subsequence 289822.py
@@ -0,0 +1,14 @@
+# Problem: Increasing Triplet Subsequence - https://leetcode.com/problems/increasing-triplet-subsequence/
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        f = float("inf")
+        s = float("inf")
+        for num in nums:
+            if num <= f:
+                f = num
+            elif num <= s:
+                s = num
+            else:
+                return True
+        return False
