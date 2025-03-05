980463904878c6f493d7122900b10390099137f4
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-03-05 17:16:34 +0300

    Created 'The Maximum Number' by A2SV Hub

---
+++ 05-Mar-2025/The Maximum Number 292969.py
@@ -0,0 +1,6 @@
+# Problem: The Maximum Number - https://leetcode.com/problems/third-maximum-number/description/
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        uni = sorted(set(nums), reverse=True) 
+        return uni[2] if len(uni) >= 3 else uni[0] 
