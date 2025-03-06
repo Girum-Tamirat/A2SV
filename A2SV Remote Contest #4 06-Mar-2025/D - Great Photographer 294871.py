4a88fd08453a9ccee51b84909e06109f4261bf53
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-03-06 18:54:13 +0300

    Created 'D - Great Photographer' by A2SV Hub

---
+++ A2SV Remote Contest #4 06-Mar-2025/D - Great Photographer 294871.py
@@ -0,0 +1,23 @@
+# Problem: D - Great Photographer - https://codeforces.com/gym/590201/problem/D
+
+n, x0 = map(int, input().split())
+starts = []
+ends = []
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    starts.append(min(a, b))
+    ends.append(max(a, b))
+
+max_start = max(starts)
+min_end = min(ends)
+
+if max_start > min_end:
+    print(-1)
+else:
+    if x0 < max_start:
+        print(max_start - x0)
+    elif x0 > min_end:
+        print(x0 - min_end)
+    else:
+        print(0)
