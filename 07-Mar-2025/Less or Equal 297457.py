1f5762edd578eddd81ddb8f7fecf74657db42762
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-03-07 22:05:06 +0300

    Created 'Less or Equal' by A2SV Hub

---
+++ 07-Mar-2025/Less or Equal 297457.py
@@ -0,0 +1,14 @@
+# Problem: Less or Equal - https://codeforces.com/problemset/problem/977/C
+
+def find_x(n, k, arr):
+    arr.sort()
+    if k == 0:
+        return arr[0] - 1 if arr[0] > 1 else -1
+    x = arr[k-1]
+    if k < n and arr[k] == x:
+        return -1
+    return x
+
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+print(find_x(n, k, arr))
