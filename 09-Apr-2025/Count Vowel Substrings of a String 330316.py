786a07f1e2477c968e1d40c57d491435641d0441
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-04-10 00:24:37 +0300

    Created 'Count Vowel Substrings of a String' by A2SV Hub

---
+++ 09-Apr-2025/Count Vowel Substrings of a String 330316.py
@@ -0,0 +1,18 @@
+# Problem: Count Vowel Substrings of a String - https://leetcode.com/problems/count-vowel-substrings-of-a-string/description/
+
+class Solution:
+    def countVowelSubstrings(self, word: str) -> int:
+        n = len(word)
+        vowels = set('aeiou')
+        count = 0
+        for i in range(n):
+            if word[i] not in vowels:
+                continue
+            seen = set()
+            for j in range(i, n):
+                if word[j] not in vowels:
+                    break
+                seen.add(word[j])
+                if len(seen) == 5:
+                    count += 1
+        return count
