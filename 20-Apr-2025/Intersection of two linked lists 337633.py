d1934ff039a27760d925da85ea253c86c02fff0b
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-04-20 08:25:10 +0300

    Created 'Intersection of two linked lists' by A2SV Hub

---
+++ 20-Apr-2025/Intersection of two linked lists 337633.py
@@ -0,0 +1,17 @@
+# Problem: Intersection of two linked lists - https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        if not headA or not headB:
+            return None
+        h1, h2 = headA, headB
+        while h1 != h2:
+            h1 = h1.next if h1 else headB
+            h2 = h2.next if h2 else headA
+        return h1
