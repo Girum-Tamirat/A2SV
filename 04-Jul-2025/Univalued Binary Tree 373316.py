45446258ddfe793330368b3cf57e152c9e6c9eac
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-07-04 18:32:27 +0300

    Created 'Univalued Binary Tree' by A2SV Hub

---
+++ 04-Jul-2025/Univalued Binary Tree 373316.py
@@ -0,0 +1,21 @@
+# Problem: Univalued Binary Tree - https://leetcode.com/problems/univalued-binary-tree/description/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
+        res, queue= [], deque([root])
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left: queue.append(node.left)
+                if node.right: queue.append(node.right)
+            res.append(level)
+        a = [i for j in res for i in j]
+        return all(x==a[0] for x in a)
