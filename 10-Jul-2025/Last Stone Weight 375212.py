97061d6587b5784adc53480956d61e72f8f3f784
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-07-10 22:01:32 +0300

    Created 'Last Stone Weight' by A2SV Hub

---
+++ 10-Jul-2025/Last Stone Weight 375212.py
@@ -0,0 +1,12 @@
+# Problem: Last Stone Weight - https://leetcode.com/problems/last-stone-weight/
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        stones=[-stone for stone in stones]
+        heapq.heapify(stones)
+        while len(stones)>1:
+            s1 = -heapq.heappop(stones)
+            s2 = -heapq.heappop(stones)
+            if s1!=s2:
+                heapq.heappush(stones, -(s1-s2))
+        return -stones[0] if stones else 0
