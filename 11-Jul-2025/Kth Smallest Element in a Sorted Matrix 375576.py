b18e7a93a8b9b0eca49ebf84ed51924801c23130
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-07-11 21:48:49 +0300

    Created 'Kth Smallest Element in a Sorted Matrix' by A2SV Hub

---
+++ 11-Jul-2025/Kth Smallest Element in a Sorted Matrix 375576.py
@@ -0,0 +1,6 @@
+# Problem: Kth Smallest Element in a Sorted Matrix - https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/description/
+
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        m = sorted([j for i in matrix for j in i])
+        return m[k-1]
