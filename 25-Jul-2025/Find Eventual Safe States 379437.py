00bed9877e4c3eeda5e0c2b404609ca9d3adff11
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-07-25 23:09:06 +0300

    Created 'Find Eventual Safe States' by A2SV Hub

---
+++ 25-Jul-2025/Find Eventual Safe States 379437.py
@@ -0,0 +1,19 @@
+# Problem: Find Eventual Safe States - https://leetcode.com/problems/find-eventual-safe-states/
+
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        safe = {}
+        def dfs(i):
+            if i in safe:
+                return safe[i]
+            safe[i] = False
+            for nei in graph[i]:
+                if not dfs(nei):
+                    return safe[i]
+            safe[i] = True
+            return safe[i]
+        res = []
+        for i in range(len(graph)):
+            if dfs(i):
+                res.append(i)
+        return res
