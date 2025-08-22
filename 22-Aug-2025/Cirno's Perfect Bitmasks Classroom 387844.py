60127741ba95ad06264b627cc959775acd99a022
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-08-22 20:42:00 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 22-Aug-2025/Cirno's Perfect Bitmasks Classroom 387844.py
@@ -0,0 +1,11 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+n = int(input())
+for _ in range(n):
+    a = int(input())
+    if a == 1:
+        print(3)
+    elif a & (a - 1) == 0:
+        print(a + 1)
+    else:
+        print(a & -a)
