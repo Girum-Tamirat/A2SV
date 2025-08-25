8c3740827bf3936d1a121d527fa984a78089d76d
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-08-25 21:19:24 +0300

    Created 'Sum of Two Integers' by A2SV Hub

---
+++ 25-Aug-2025/Sum of Two Integers 388798.py
@@ -0,0 +1,11 @@
+# Problem: Sum of Two Integers - https://leetcode.com/problems/sum-of-two-integers/description/
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        MASK = 0xFFFFFFFF
+        MAX_INT = 0x7FFFFFFF
+        while b!=0:
+            c = (a & b) & MASK
+            a = (a ^ b) & MASK
+            b = (c << 1) & MASK
+        return a if a <= MAX_INT else ~(a ^ MASK)
