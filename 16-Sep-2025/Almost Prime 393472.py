361fc545ab9ea55fd6435c64dfb85682ebf6bab7
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-09-16 11:32:05 +0300

    Created 'Almost Prime' by A2SV Hub

---
+++ 16-Sep-2025/Almost Prime 393472.py
@@ -0,0 +1,13 @@
+# Problem: Almost Prime - https://codeforces.com/problemset/problem/26/A
+
+n = int(input())
+factors = [0] * (n + 1)
+for p in range(2, n + 1):
+    if factors[p] == 0:
+        for multiple in range(p, n + 1, p):
+            factors[multiple] += 1
+count = 0
+for x in range(2, n + 1):
+    if factors[x] == 2:
+        count += 1
+print(count)
