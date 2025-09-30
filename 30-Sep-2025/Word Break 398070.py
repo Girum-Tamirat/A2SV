87cf702f642ce8d419227280e787dab14613bbfd
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-09-30 20:12:01 +0300

    Created 'Word Break' by A2SV Hub

---
+++ 30-Sep-2025/Word Break 398070.py
@@ -0,0 +1,13 @@
+# Problem: Word Break - https://leetcode.com/problems/word-break/description/
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        word_set = set(wordDict)
+        dp = [False] * (len(s) + 1)
+        dp[0] = True 
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in word_set:
+                    dp[i] = True
+                    break 
+        return dp[len(s)]
