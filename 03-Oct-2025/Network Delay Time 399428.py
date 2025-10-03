6be9d80b452106ac6175ccfef8d9eb6845449c89
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-10-03 20:13:00 +0300

    Created 'Network Delay Time' by A2SV Hub

---
+++ 03-Oct-2025/Network Delay Time 399428.py
@@ -0,0 +1,20 @@
+# Problem: Network Delay Time - https://leetcode.com/problems/network-delay-time/description/
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        graph = defaultdict(list)
+        for u, v, w in times:
+            graph[u].append((v, w))
+        heap = [(0, k)] 
+        dist = {} 
+        while heap:
+            time, node = heapq.heappop(heap)
+            if node in dist:
+                continue
+            dist[node] = time
+            for neighbor, weight in graph[node]:
+                if neighbor not in dist:
+                    heapq.heappush(heap, (time + weight, neighbor))
+        if len(dist) != n:
+            return -1
+        return max(dist.values())
