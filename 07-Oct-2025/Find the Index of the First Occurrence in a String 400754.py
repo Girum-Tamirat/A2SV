9a5567f11076284ae96d014eead09e5870375ea8
Girum Tamirat Fuchune <141916037+Girum-Tamirat@users.noreply.github.com>
2025-10-07 23:12:04 +0300

    Created 'Find the Index of the First Occurrence in a String' by A2SV Hub

---
+++ 07-Oct-2025/Find the Index of the First Occurrence in a String 400754.py
@@ -0,0 +1,7 @@
+# Problem: Find the Index of the First Occurrence in a String - https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+
+class Solution:
+    def strStr(self, h: str, n: str) -> int:
+        for i in range(len(h)-len(n)+1):
+            if h[i:i+len(n)]==n: return i
+        return -1
